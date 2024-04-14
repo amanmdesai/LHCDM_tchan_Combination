26859bd7df4cd0d486eeea0bbb0e8baaa5571fab
amanmdesai <amanmukeshdesai@gmail.com>
2024-04-14 21:49:58 +0930

    update

---
+++ install.sh
@@ -0,0 +1,44 @@
+###################################
+#### INSTALL AND CONFIGURE MA5 ####
+###################################
+InstallMadAnalysisfolder="/home/amdesai/Music/LHCDM_tchan_Combination"
+
+#/MG5MA5/MA5"
+
+if [[ ! -e ${InstallMadAnalysisfolder}/MA5installed ]]; then
+  if [[ -d ${InstallMadAnalysisfolder}/madanalysis5 ]]; then
+    echo "Removing previous MA5 installation"
+    rm -rf ${InstallMadAnalysisfolder}/madanalysis5
+  fi
+
+echo "Installing MA5"
+mkdir -p ${InstallMadAnalysisfolder}
+cd ${InstallMadAnalysisfolder}
+git clone https://github.com/MadAnalysis/madanalysis5.git
+
+python3 -m venv py3_env
+source py3_env/bin/activate
+pip3 install --upgrade pip
+python3 -m pip install scipy click tqdm six jsonschema jsonpatch PyYAML pyhf
+#   deactivate
+
+  # bug fix: modify the ma5 executable to work with python > 3.9 (see https://github.com/MadAnalysis/madanalysis5/issues/237)
+  # if fixed in new releases this has no effect
+  sed -i "s|import importlib|from importlib import util|g"                               ${InstallMadAnalysisfolder}/madanalysis5/bin/ma5
+  sed -i "s|if not importlib.util.find_spec(\"six\"):|if not util.find_spec(\"six\"):|g" ${InstallMadAnalysisfolder}/madanalysis5/bin/ma5
+
+  cd ${InstallMadAnalysisfolder}/madanalysis5
+  python3 bin/ma5 -s < $InstallMadAnalysisfolder/MA5_installpackages
+
+  cd $InstallMadAnalysisfolder
+
+  # bug fix to avoid the recast crashing for some searches
+  # if fixed in new releases this has no effect
+  #sed -i "s|new_sigma = new_sigma \* lumi_scaling\*\*2|new_sigma = sigma\*lumi_scaling\*\*2|g" ${InstallMadAnalysisfolder}/madanalysis5/madanalysis/misc/run_recast.py
+
+  if [[ -d ${InstallMadAnalysisfolder}/madanalysis5/tools/PADForSFS ]] && [[ -d ${InstallMadAnalysisfolder}/madanalysis5/tools/PAD ]]; then
+    touch ${InstallMadAnalysisfolder}/MA5installed
+  fi
+fi
+
+
