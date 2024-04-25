bdfee1e67009ee86938ded68ecb2dd2d4a8169d3
amanmdesai <amanmukeshdesai@gmail.com>
2024-04-25 10:30:33 +0930

    add code for full combination

--- NLOCombination_XX.py
+++ NLOCombination_XX.py
@@ -15,13 +15,11 @@ def extract_tar(tar_file, destination):
 # define point
 mY = 2800
 mX = 1200
-proc = 'Full'
+proc = 'XX'
 order = 'NLO'
 couplings = [3.5,5.0]
 coupling = 5.0
 coupling_power = {'XX' : 4, 'XY':2, 'YYi':2, 'YYQCD': 0, 'YYtPP': 4, 'YYtPM': 4, 'YYtMM': 4}
-processes_full = ['XX','XY','YYi','YYQCD','YYtPP','YYtPM','YYtMM']
-
 luminosity=137
 
 quark = 'u'
@@ -65,18 +63,16 @@ for col in float_cols:
     #print(col,df[col])
     df[col] = df[col].astype(float)
 
-select_row  = df[(df["my(GeV)"] == mY) & (df["mx(GeV)"] == mX)  & (df['order'] == order)]
+select_XXrow  = df[(df["my(GeV)"] == mY) & (df["mx(GeV)"] == mX) & (df['process']==proc) & (df['order'] == order)]
 
-if select_row.empty:
+if select_XXrow.empty:
     print("no point found")
     sys.exit()
 
 rescale_xsec = {}
 
 for coup in couplings:
-    for proc in processes_full: 
-        rescale_xsec[coup] = 0
-        rescale_xsec[coup]+=select_row['CShat(pb)'].values[0]*coup**coupling_power[proc]
+    rescale_xsec[coup]=select_XXrow['CShat(pb)'].values[0]*coup**coupling_power[proc]
 
 print(rescale_xsec)
 
@@ -133,9 +129,7 @@ print(f"has PAD: {main.session_info.has_pad}\n"
 
 ma5.BackendManager.set_madanalysis_backend("/eos/user/a/aman/LHCDM_tchan_Combination/madanalysis5")
 
-main_path = folderName
-
-#"/home/amdesai/Music/LHCDM_tchan_Combination/installed_files/Sigmas"
+main_path = folderName#"/home/amdesai/Music/LHCDM_tchan_Combination/installed_files/Sigmas"
 
 # Output folder
 combined_path = "/eos/user/a/aman/LHCDM_tchan_Combination/output/" + name_recast_file 
@@ -143,18 +137,18 @@ if not os.path.isdir(combined_path):
     os.mkdir(combined_path)
 
 # Samples to be combined. Each set of samples are generated and stored in separate directories
-proc_path  = os.path.join(main_path, "MA5_Recast/S3M_XX_NLO_SMu_MY2800_MX1200_recast/Output/SAF/dmtsimp/cms_sus_19_006/Cutflows")
+XX_path  = os.path.join(main_path, "MA5_Recast/S3M_XX_NLO_SMu_MY2800_MX1200_recast/Output/SAF/dmtsimp/cms_sus_19_006/Cutflows")
 
-xsec_proc  = rescale_xsec[coupling]
+xsec_XX  = rescale_xsec[coupling]
 
-proc_collection  = ma5.cutflow.Collection(proc_path,  xsection = xsec_proc,  lumi = luminosity,)
+XX_collection  = ma5.cutflow.Collection(XX_path,  xsection = xsec_XX,  lumi = luminosity,)
 
-xsec = xsec_proc
+xsec = xsec_XX
 
 if not os.path.isdir(combined_path):
     os.mkdir(combined_path)
 
-with open(os.path.join(combined_path,'sample_info_proc.json'),'w') as info_file:
+with open(os.path.join(combined_path,'sample_info_XX.json'),'w') as info_file:
     info = {"xsec" : float(xsec)}
     json.dump(info, info_file, indent = 4)
 
@@ -174,7 +168,7 @@ lumi, regions, regiondata = run_recast.parse_info_file(ET,analysis,extrapolated_
 
 # Reset signal region yields for combined sample
 for reg in regions:
-    regiondata[reg]["Nf"] = proc_collection[reg].final_cut.eff * xsec_proc  
+    regiondata[reg]["Nf"] = XX_collection[reg].final_cut.eff * xsec_XX  
     regiondata[reg]["N0"] = xsec
     
 # Calculate exclusion limits
