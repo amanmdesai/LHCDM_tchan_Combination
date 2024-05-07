fb3dca5edab36488e8406c40581a2bd34fa1e5b9
amanmdesai <amanmukeshdesai@gmail.com>
2024-05-07 18:58:22 +0930

    add argparse statements

--- NLOCombination_v2.py
+++ NLOCombination_v2.py
@@ -4,13 +4,16 @@ import logging
 import sys
 import ma5_expert as ma5
 import pandas as pd
+import tarfile
 import shutil
 import os
 
 
-# Test command to run
-# python NLOCombination.py --MY 1300 --MX 900 --coup 5 --quark d --order NLO --model F3S 
-
+"""
+def extract_tar(tar_file, destination):
+    with tarfile.open(tar_file, 'r:gz') as tar:
+        tar.extractall(destination)
+"""
 
 def extract_tar(tar_gz_file, extract_path):
     try:
@@ -19,44 +22,6 @@ def extract_tar(tar_gz_file, extract_path):
     except Exception as e:
         print(f"Error extracting '{tar_gz_file}': {e}")
 
-
-parser = argparse.ArgumentParser(prog = 'DMSimpt_Combination',description = '')
-parser.add_argument("--MY", type=int,help='mass of DM particle')
-parser.add_argument("--MX", type=int,help='mass of DM mediator')
-parser.add_argument("--coup", type=float,help='Coupling')
-parser.add_argument("--quark", type=str,help='Quark')
-parser.add_argument("--order", type=str,help='order')
-parser.add_argument("--model", type=str,help='Model')
-parser.add_argument("--input", type=str,help='Input file path and Name',default="/eos/user/a/aman/dsb_lowstat/")
-parser.add_argument("--output", type=str,help='Output file path and Name',default="/eos/user/a/aman/LHCDM_tchan_Combination/output/")
-parser.add_argument("--ma5dir", type=str,help='Input file path and Name',default="/eos/user/a/aman/LHCDM_tchan_Combination/madanalysis5")
-args = parser.parse_args()
-
-# define point
-
-mY = args.MY
-mX = args.MX
-order = args.order
-coupling = args.coup
-quark = args.quark
-model = args.model
-proc_study = 'Full'
-
-inputfolder = args.input #"/eos/user/a/aman/dsb_lowstat/"
-folderName = inputfolder + "Results_{}_recast".format(model)
-fileName = "Sigmas/{}_sigmas.dat".format(model)
-
-
-print(mY, mX, order, coupling, quark, model)
-
-"""
-mY = 1300
-mX = 900
-proc_study = 'Full'
-order = 'NLO'
-coupling = 5.0
-quark = 'd'
-model = 'F3S'
 
 
 
@@ -67,16 +32,22 @@ proc_study = 'Full'
 order = 'NLO'
 couplings = [3.5,5.0]
 coupling = 5.0
-quark = 'd'
-model = 'F3S'
-"""
-
 coupling_power = {'XX' : 4, 'XY':2, 'YYi':2, 'YYQCD': 0, 'YYtPP': 4, 'YYtPM': 4, 'YYtMM': 4}
 processes_full = ['XX','XY','YYi','YYQCD','YYtPP','YYtPM','YYtMM']
 PAD4SFS = ["atlas_exot_2018_06"]
-analysis_names = ["atlas_conf_2019_040","atlas_exot_2018_06",]
+analysis_names = ["atlas_conf_2019_040","atlas_exot_2018_06",]#"cms_exo_20_004","cms_sus_19_006"]
+
 
 luminosity=137
+
+quark = 'd'
+model = 'F3S'
+
+#folderName = "/eos/user/a/aman/SamplesForAman/u/Results_F3S_recast"
+#fileName = "Sigmas/F3S_sigmas.dat"
+
+folderName = "/eos/user/a/aman/dsb_lowstat/Results_F3S_recast"
+fileName = "Sigmas/F3S_sigmas.dat"
 
 inputfile = os.path.join(folderName,fileName)
 
@@ -117,33 +88,33 @@ for col in float_cols:
 select_row  = df[(df["my(GeV)"] == mY) & (df["mx(GeV)"] == mX)  & (df['order'] == order)]
 select_row_YYi  = df[(df["my(GeV)"] == mY) & (df["mx(GeV)"] == mX)]
 
-
 if select_row.empty:
     print("no point found")
     sys.exit()
 
 processes_full = ['XX','XY','YYi','YYQCD','YYtPP','YYtPM','YYtMM']
 
-rescale_xsec_XX = 0
-rescale_xsec_XY = 0
-rescale_xsec_YYi = 0
-rescale_xsec_YYQCD = 0
-rescale_xsec_YYtPP = 0
-rescale_xsec_YYtPM = 0
-rescale_xsec_YYtMM = 0
+rescale_xsec_XX = {}
+rescale_xsec_XY = {}
+rescale_xsec_YYi = {}
+rescale_xsec_YYQCD = {}
+rescale_xsec_YYtPP = {}
+rescale_xsec_YYtPM = {}
+rescale_xsec_YYtMM = {}
 
-rescale_xsec_XX=select_row[select_row["process"] == 'XX']['CShat(pb)'].values[0]*coupling**coupling_power['XX']
-rescale_xsec_XY=select_row[select_row["process"] == 'XY']['CShat(pb)'].values[0]*coupling**coupling_power['XY']
-rescale_xsec_YYi=select_row_YYi[select_row_YYi["process"] == 'YYi']['CShat(pb)'].values[0]*coupling**coupling_power['YYi']
-rescale_xsec_YYQCD=select_row[select_row["process"] == 'YYQCD']['CShat(pb)'].values[0]*coupling**coupling_power['YYQCD']
-rescale_xsec_YYtPP=select_row[select_row["process"] == 'YYtPP']['CShat(pb)'].values[0]*coupling**coupling_power['YYtPP']
-rescale_xsec_YYtPM=select_row[select_row["process"] == 'YYtPM']['CShat(pb)'].values[0]*coupling**coupling_power['YYtPM']
-rescale_xsec_YYtMM=select_row[select_row["process"] == 'YYtMM']['CShat(pb)'].values[0]*coupling**coupling_power['YYtMM']
+for coup in couplings:
+    rescale_xsec_XX[coup]=select_row[select_row["process"] == 'XX']['CShat(pb)'].values[0]*coup**coupling_power['XX']
+    rescale_xsec_XY[coup]=select_row[select_row["process"] == 'XY']['CShat(pb)'].values[0]*coup**coupling_power['XY']
+    rescale_xsec_YYi[coup]=select_row_YYi[select_row_YYi["process"] == 'YYi']['CShat(pb)'].values[0]*coup**coupling_power['YYi']
+    rescale_xsec_YYQCD[coup]=select_row[select_row["process"] == 'YYQCD']['CShat(pb)'].values[0]*coup**coupling_power['YYQCD']
+    rescale_xsec_YYtPP[coup]=select_row[select_row["process"] == 'YYtPP']['CShat(pb)'].values[0]*coup**coupling_power['YYtPP']
+    rescale_xsec_YYtPM[coup]=select_row[select_row["process"] == 'YYtPM']['CShat(pb)'].values[0]*coup**coupling_power['YYtPM']
+    rescale_xsec_YYtMM[coup]=select_row[select_row["process"] == 'YYtMM']['CShat(pb)'].values[0]*coup**coupling_power['YYtMM']
 
 
 # madanalysis expert mode 
 
-ma5dir = args.ma5dir 
+ma5dir = "/eos/user/a/aman/LHCDM_tchan_Combination/madanalysis5"#ma5dir
 if not os.path.isdir(ma5dir):
     sys.exit('Detected MadAnalysis 5 general folder is not correct: ' + ma5dir)
 os.environ['MA5_BASE']=ma5dir
@@ -183,11 +154,14 @@ print(f"has PAD: {main.session_info.has_pad}\n"
       f"has scipy {main.session_info.has_scipy}\n"
       f"has pyhf {main.session_info.has_pyhf}")
 
-ma5.BackendManager.set_madanalysis_backend(args.ma5dir)
+ma5.BackendManager.set_madanalysis_backend("/eos/user/a/aman/LHCDM_tchan_Combination/madanalysis5")
 
 main_path = folderName
 
+#"/home/amdesai/Music/LHCDM_tchan_Combination/installed_files/Sigmas"
+
 # Samples to be combined. Each set of samples are generated and stored in separate directories
+
 
 for proc in processes_full:
 
@@ -196,7 +170,7 @@ for proc in processes_full:
     else:
         order_file = order
 
-    name_recast_file = model + "_" + proc + "_" + order_file + "_SM"+ quark + "_MY" + str(mY) + "_MX" + str(mX) + "_recast"
+    name_recast_file = model + "_" + proc + "_" + order_file + "_SMd_" + "MY" + str(mY) + "_MX" + str(mX) + "_recast"
 
     file = os.path.join(folderName, "MA5_Recast",name_recast_file+".tar.gz")
     extract_tar(file, os.path.join(folderName, "MA5_Recast",))
@@ -212,36 +186,36 @@ for proc in processes_full:
 # Output folder
 
 if proc_study == "Full":
-    combined_path = args.output + model + "_" + proc + "_" + order_file + "_SM"+ quark+ "_MY" + str(mY) + "_MX" + str(mX) + "_recast" 
+    combined_path = "/eos/user/a/aman/LHCDM_tchan_Combination/output/" + model + "_" + proc + "_" + order_file + "_SMd_" + "MY" + str(mY) + "_MX" + str(mX) + "_recast" 
 else:
-    combined_path = args.output + name_recast_file 
+    combined_path = "/eos/user/a/aman/LHCDM_tchan_Combination/output/" + name_recast_file 
 
 if not os.path.isdir(combined_path):
     os.mkdir(combined_path)
 
 
-#for coupling in couplings:
+#for coup in couplings:
 for ana in analysis_names:
-    XX_path  = os.path.join(main_path, "MA5_Recast/{}_XX_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    XY_path  = os.path.join(main_path, "MA5_Recast/{}_XY_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    YYi_path  = os.path.join(main_path, "MA5_Recast/{}_YYi_LO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    YYQCD_path  = os.path.join(main_path, "MA5_Recast/{}_YYQCD_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    YYtPP_path  = os.path.join(main_path, "MA5_Recast/{}_YYtPP_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    YYtPM_path  = os.path.join(main_path, "MA5_Recast/{}_YYtPM_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
-    YYtMM_path  = os.path.join(main_path, "MA5_Recast/{}_YYtMM_NLO_SM{}_MY{}_MX{}_recast/Output/SAF/dmtsimp/{}/Cutflows".format(model, quark, mY, mX, ana))
+    XX_path  = os.path.join(main_path, "MA5_Recast/F3S_XX_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    XY_path  = os.path.join(main_path, "MA5_Recast/F3S_XY_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    YYi_path  = os.path.join(main_path, "MA5_Recast/F3S_YYi_LO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    YYQCD_path  = os.path.join(main_path, "MA5_Recast/F3S_YYQCD_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    YYtPP_path  = os.path.join(main_path, "MA5_Recast/F3S_YYtPP_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    YYtPM_path  = os.path.join(main_path, "MA5_Recast/F3S_YYtPM_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
+    YYtMM_path  = os.path.join(main_path, "MA5_Recast/F3S_YYtMM_NLO_SMd_MY1300_MX900_recast/Output/SAF/dmtsimp/{}/Cutflows".format(ana))
 
-    xsec_proc  = rescale_xsec_XX + rescale_xsec_XY + rescale_xsec_YYi + rescale_xsec_YYQCD + \
-    rescale_xsec_YYtPP + rescale_xsec_YYtPM + rescale_xsec_YYtMM
+    xsec_proc  = rescale_xsec_XX[coupling] + rescale_xsec_XY[coupling] + rescale_xsec_YYi[coupling] + rescale_xsec_YYQCD[coupling] + \
+    rescale_xsec_YYtPP[coupling] + rescale_xsec_YYtPM[coupling] + rescale_xsec_YYtMM[coupling]
     
     xsec = xsec_proc
 
-    XX_collection  = ma5.cutflow.Collection(XX_path,  xsection = rescale_xsec_XX,  lumi = luminosity,)
-    XY_collection  = ma5.cutflow.Collection(XY_path,  xsection = rescale_xsec_XY,  lumi = luminosity,)
-    YYi_collection  = ma5.cutflow.Collection(YYi_path,  xsection = rescale_xsec_YYi,  lumi = luminosity,)
-    YYQCD_collection = ma5.cutflow.Collection(YYQCD_path, xsection = rescale_xsec_YYQCD, lumi = luminosity,)
-    YYtPP_collection = ma5.cutflow.Collection(YYtPP_path, xsection = rescale_xsec_YYtPP, lumi = luminosity,)
-    YYtPM_collection = ma5.cutflow.Collection(YYtPM_path, xsection = rescale_xsec_YYtPM, lumi = luminosity,)
-    YYtMM_collection = ma5.cutflow.Collection(YYtMM_path, xsection = rescale_xsec_YYtMM, lumi = luminosity,)
+    XX_collection  = ma5.cutflow.Collection(XX_path,  xsection = rescale_xsec_XX[coupling],  lumi = luminosity,)
+    XY_collection  = ma5.cutflow.Collection(XY_path,  xsection = rescale_xsec_XY[coupling],  lumi = luminosity,)
+    YYi_collection  = ma5.cutflow.Collection(YYi_path,  xsection = rescale_xsec_YYi[coupling],  lumi = luminosity,)
+    YYQCD_collection = ma5.cutflow.Collection(YYQCD_path, xsection = rescale_xsec_YYQCD[coupling], lumi = luminosity,)
+    YYtPP_collection = ma5.cutflow.Collection(YYtPP_path, xsection = rescale_xsec_YYtPP[coupling], lumi = luminosity,)
+    YYtPM_collection = ma5.cutflow.Collection(YYtPM_path, xsection = rescale_xsec_YYtPM[coupling], lumi = luminosity,)
+    YYtMM_collection = ma5.cutflow.Collection(YYtMM_path, xsection = rescale_xsec_YYtMM[coupling], lumi = luminosity,)
 
     if not os.path.isdir(combined_path):
         os.mkdir(combined_path)
@@ -256,7 +230,7 @@ for ana in analysis_names:
     extrapolated_lumi = "default"; analysis = ana
     outfile = os.path.join(combined_path, 'CLs_output.dat')
 
-    run_recast = RunRecast(main, "{}_XX/{}".format(model, ana))
+    run_recast = RunRecast(main, "F3S_XX/{}".format(ana))
     if ana in PAD4SFS:
         run_recast.pad = os.path.join(ma5dir, "tools/PADForSFS")
     else:
@@ -270,13 +244,13 @@ for ana in analysis_names:
 
     # Reset signal region yields for combined sample
     for reg in regions:
-        regiondata[reg]["Nf"] = XX_collection[reg].final_cut.eff * rescale_xsec_XX + \
-                                XY_collection[reg].final_cut.eff * rescale_xsec_XY + \
-                                YYQCD_collection[reg].final_cut.eff * rescale_xsec_YYQCD + \
-                                YYi_collection[reg].final_cut.eff * rescale_xsec_YYi + \
-                                YYtPP_collection[reg].final_cut.eff * rescale_xsec_YYtPP + \
-                                YYtPM_collection[reg].final_cut.eff * rescale_xsec_YYtPM + \
-                                YYtMM_collection[reg].final_cut.eff * rescale_xsec_YYtMM 
+        regiondata[reg]["Nf"] = XX_collection[reg].final_cut.eff * rescale_xsec_XX[coupling] + \
+                                XY_collection[reg].final_cut.eff * rescale_xsec_XY[coupling] + \
+                                YYQCD_collection[reg].final_cut.eff * rescale_xsec_YYQCD[coupling] + \
+                                YYi_collection[reg].final_cut.eff * rescale_xsec_YYi[coupling] + \
+                                YYtPP_collection[reg].final_cut.eff * rescale_xsec_YYtPP[coupling] + \
+                                YYtPM_collection[reg].final_cut.eff * rescale_xsec_YYtPM[coupling] + \
+                                YYtMM_collection[reg].final_cut.eff * rescale_xsec_YYtMM[coupling] 
         regiondata[reg]["N0"] = xsec
     #regiondata[reg]["nb"]=extrapolated_lumi/luminosity*regiondata[reg]["nb"]
     # Calculate exclusion limits
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd /eos/user/a/aman/LHCDM_tchan_Combination
 source py3_env/bin/activate
-python NLOCombination.py
+python NLOCombination.py --MY 1300 --MX 900 --coup 5 --quark d --order NLO --model F3S 
+#python NLOCombination.py
