557b8077943dd1bc1300dca06bf4ed735563ac2b
amanmdesai <amanmukeshdesai@gmail.com>
2024-05-09 11:28:14 +0930

    update creator file

--- job_creator.py
+++ job_creator.py
@@ -1,20 +1,29 @@
 import os
 import argparse
 
-
 parser = argparse.ArgumentParser(prog = 'DMSimpt_Combination',description = '')
-parser.add_argument("--input", type=str,help='Input file path and Name',default="/eos/user/a/aman/dsb_lowstat/")
+parser.add_argument("--input", type=str,help='Input Data file path and Name',default="/eos/user/a/aman/dsb_lowstat/")
+parser.add_argument("--programpath", type=str,help='path to NLOCombination.py',default="/eos/user/a/aman/LHCDM_tchan_Combination/")
 parser.add_argument("--output", type=str,help='Output file path and Name',default="/eos/user/a/aman/LHCDM_tchan_Combination/output/")
 parser.add_argument("--ma5dir", type=str,help='Input file path and Name',default="/eos/user/a/aman/LHCDM_tchan_Combination/madanalysis5")
-parser.add_argument("--wmratio", type=str,help='yes/no for fixed wy/my ratio',default="no")
+parser.add_argument("--wmratio", type=str,help='yes/no for fixed wy/my ratio',default="yes")
 args = parser.parse_args()
 
+MODELArray = ["F3S"]
+YMASSArray = [1300]
+XMASSArray = [900]
+COUPLINGArray = [5]
+QuarkArray = ["d"]
+OrderArray = ["NLO"]
+
+"""
 MODELArray = ["S3D", "S3M", "F3S", "F3C", "F3V", "F3W"]
 XMASSArray = [10, 50]
 YMASSArray = [100, 200]
 COUPLINGArray = [5]
 QuarkArray = ["d"]
 OrderArray = ["LO", "NLO"]
+"""
 
 for my in YMASSArray:
     for mx in XMASSArray:
@@ -26,15 +35,16 @@ for my in YMASSArray:
                             job_name = "{}_SM{}_mY{}_mX{}_coup{}.sub".format(model, quark, my, mx, coup)
                             run_name = "{}_SM{}_mY{}_mX{}_coup{}.sh".format(model, quark, my, mx, coup)
                             os.system("rm " + run_name)
-                            os.system("echo cd /eos/user/a/aman/LHCDM_tchan_Combination>> {}".format(run_name))
+                            os.system("echo \#!/bin/bash >> {}".format(run_name))
+                            os.system("echo cd /eos/user/a/aman/LHCDM_tchan_Combination >> {}".format(run_name))
                             os.system("echo source py3_env/bin/activate >> {}".format(run_name))
-                            os.system("echo python NLOCombination.py --MY {} --MX {} --coup {} --quark {} --order NLO --model {} --input {} --output {} --wmratio {} >> {}".format(my, mx, coup, quark, model, args.input, args.output, args.wmratio, run_name))
+                            os.system("echo python {}/NLOCombination.py --MY {} --MX {} --coup {} --quark {} --order NLO --model {} --input {} --output {} --wmratio {} >> {}".format(args.programpath, my, mx, coup, quark, model, args.input, args.output, args.wmratio, run_name))
                             os.system("rm " + job_name)
                             os.system("touch " + job_name)
                             os.system("echo Executable            = {}>> {}".format(run_name, job_name))
-                            #os.system(f"echo Output                = log/ap.$(ClusterId).$(ProcId).out>> {job_name}")
-                            #os.system(f"echo Error                 = log/ap.$(ClusterId).$(ProcId).err>> {job_name}")
-                            #os.system(f"echo Log                   = log/ap.$(ClusterId).log>> {job_name}")
+                            os.system("echo Output                = log/ap.{}.out >> {}".format(job_name,job_name))
+                            os.system("echo Error                 = log/ap.{}.err >> {}".format(job_name,job_name))
+                            os.system("echo Log                   = log/ap.{}.log >> {}".format(job_name,job_name))
                             os.system("echo should_transfer_files   = yes>> {}".format(job_name))
                             os.system("echo RequestCpus = 8 >> {}".format(job_name))
                             os.system("echo +JobFlavour = 'testmatch' >> {}".format(job_name))
