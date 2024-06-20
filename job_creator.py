10cd462fc88159443f9b186a382682dc7cf538d0
amanmdesai <amanmukeshdesai@gmail.com>
2024-06-21 05:55:30 +0930

    update

--- job_creator.py
+++ job_creator.py
@@ -15,32 +15,36 @@ args = parser.parse_args()
 MODELArray = [args.model]
 XMASSArray = [-5, -1, 0, 1, 10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900, 4000]
 YMASSArray = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900, 4000]
-COUPLINGArray = [1.0]#, 3.5]
+COUPLINGArray = [1.0]
 QuarkArray = [args.quark]
 OrderArray = ["LO","NLO"]
 processArray = ["XX", "XY", "YYQCD", "YYt", "YYSum", "Full"]
 
-#MODELArray = ["S3M"]#, "F3S", "F3V"]
-#QuarkArray = ["d", "s"]#, "u"]
-
-
 os.system(f"mkdir -p {args.model}_{args.quark}")
 
-
 for model in MODELArray:
-    if model == "S3M" and quark == "u":
-        COUPLINGArray = [1.0, 3.5]
-    elif model == "F3S" and quark == "u":
-        COUPLINGArray = [1.0, 4.8]
-    else: 
-        COUPLINGArray = [1.0]
-    for coup in COUPLINGArray:  
-        for my in YMASSArray:
-            for mx in XMASSArray:
-                for quark in QuarkArray:
+    for quark in QuarkArray:
+        if model == "S3M" and quark == "u":
+            COUPLINGArray = [1.0, 3.5]
+        elif model == "F3S" and quark == "u":
+            COUPLINGArray = [1.0, 4.8]
+        else: 
+            COUPLINGArray = [1.0]
+        for coup in COUPLINGArray:  
+            for my in YMASSArray:
+                for mx in XMASSArray:
+                    if mx < 0:
+                        mx = mx + my
+                    if mx >= my:
+                        continue
                     os.system(f"mkdir -p log_{quark}_{model}")
                     for order in OrderArray:
                         for proc in processArray:
+                            if proc != "Full":
+                                proccheck = proc
+                            else:
+                                proccheck = "XX"
+
                             print("")
                             print("")
                             print("+-------------------------------------------------------+")
@@ -51,17 +55,8 @@ for model in MODELArray:
                             print("")
                             print("")
 
-                            #print("model", "coup", "mY", "mX", "quark", "order", "proc")
-                            #print(model, coup, my, mx, quark, order, proc)
-                            if mx < 0:
-                                mx = mx + my
-                            if mx >= my:
-                                continue
-                            if proc != "Full":
-                                proccheck = proc
-                            else:
-                                proccheck = "XX"
                             print("file : ", os.path.join(args.input, "Results_"+model+"_recast","MA5_Recast", model + "_" + proccheck + "_" + order + "_SM"+ quark + "_MY" + str(my) + "_MX" + str(mx) + "_recast.tar.gz"))
+
                             if os.path.exists(os.path.join(args.input, "Results_"+model+"_recast","MA5_Recast", model + "_" + proccheck + "_" + order + "_SM"+ quark + "_MY" + str(my) + "_MX" + str(mx) + "_recast.tar.gz")):
                                 job_name = "{}_{}/{}_SM{}_mY{}_mX{}_proc{}_order{}_coup{}.sub".format(model, quark, model, quark, my, mx, proc, order, coup)
                                 run_name = "{}_{}/{}_SM{}_mY{}_mX{}_proc{}_order{}_coup{}.sh".format(model, quark, model, quark, my, mx, proc, order, coup)
